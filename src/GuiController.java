a23f816af3e70e284d7856a55071e7c894fc9cd5
Nico Fandrych <nicoproductivity@gmail.com>
2025-12-13 16:17:39 +0100

    Added GUI, including Main, Controller, Launcher and fxml.

---
+++ src/GuiController.java
@@ -0,0 +1,19 @@
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class GuiController {
+    @FXML
+    private ComboBox<String> comboBoxEdges;
+    @FXML
+    private ComboBox<String> comboBoxColors;
+    @FXML
+    private ComboBox<String> comboBoxRoutes;
+
+    public void initialize(){
+        comboBoxEdges.setItems(FXCollections.observableArrayList());
+        comboBoxColors.setItems(FXCollections.observableArrayList("black", "white"));
+        comboBoxRoutes.setItems(FXCollections.observableArrayList());
+    }
+
+}
---
+++ src/GuiMain.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GuiMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GuiMain.class.getResource("gui.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 708, 486);
+        stage.setTitle("Simulation");
+        stage.setScene(scene);
+        stage.show();
+        new Thread(this::roadsim, "RoadSim-Thread").start();
+    }
+
+    public void roadsim(){
+        trafficLight tl = new trafficLight("clusterJ6_J7_J8");
+        Simulation sim=new Simulation();
+        sim.start("SumoConfig/hello.sumocfg", 100);
+        for (int i = 0; i < 15; i++) {
+            Car car = new Car();
+            sim.addCar(car, "route0");
+            sim.step();
+            //sim.printCars();
+            //sim.printTrafficLights();
+        }
+        while(true){
+            sim.step();
+        }
+    }
+}
